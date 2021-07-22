2f8b80e0abf6477bdf300b4cb870445b16df62f0
YR0Kim <73688661+YR0Kim@users.noreply.github.com>
2021-07-22 11:28:20 +0900

    7/21 orderInfoController 수정

--- src/main/java/org/handmade/miniproject/order/controller/OrderInfoController.java
+++ src/main/java/org/handmade/miniproject/order/controller/OrderInfoController.java
@@ -6,7 +6,6 @@ import org.handmade.miniproject.common.dto.ListResponseDTO;
 import org.handmade.miniproject.order.dto.ListOrderInfoDTO;
 import org.handmade.miniproject.order.dto.OrderInfoDTO;
 import org.handmade.miniproject.order.dto.OrderInfoListRequestDTO;
-import org.handmade.miniproject.order.entity.OrderInfo;
 import org.handmade.miniproject.order.service.OrderInfoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,43 +30,43 @@ public class OrderInfoController {
     }
 
     // 장바구니 리스트 조회
-    @GetMapping("/cartlist")
+    @GetMapping("/cartlist?user={username}")
     public ResponseEntity<ListResponseDTO<ListOrderInfoDTO>> cartList(OrderInfoListRequestDTO orderInfoListReqeustDTO) {
         return ResponseEntity.ok(orderInfoService.getCartList(orderInfoListReqeustDTO));
     }
 
     // 리스트 상세 정보 조회, order number로 조회하므로 cartlist, orderlist 양쪽에서 사용 가능
-    @GetMapping("/list/{ono}")
+    @GetMapping("/details?ono={ono}")
     public ResponseEntity<OrderInfoDTO> getOrderDetail(@PathVariable Long ono) {
         return ResponseEntity.ok(orderInfoService.getListDetail(ono));
     }
 
     // 장바구니 삭제(=주문 완전 삭제)
-    @DeleteMapping("/cartlist/{ono}")
+    @DeleteMapping("/cartlist/delete?ono={ono}")
     public ResponseEntity<Long> deleteCart(@PathVariable Long ono) {
         return ResponseEntity.ok(orderInfoService.deleteCart(ono));
     }
 
     // 배송지 입력 및 결제 상태로 변경
-    @PutMapping("/cartList/payment")
+    @PutMapping("/cartlist/payment?ono={ono}")
     public ResponseEntity<Long> payOrder(@RequestBody OrderInfoDTO orderInfoDTO) {
         return ResponseEntity.ok(orderInfoService.payOrder(orderInfoDTO));
     }
 
     // 주문 내역 리스트 조회
-    @GetMapping("/orderlist")
+    @GetMapping("/orderlist?user={username}")
     public ResponseEntity<ListResponseDTO<ListOrderInfoDTO>> orderList(OrderInfoListRequestDTO orderInfoListRequestDTO) {
         return ResponseEntity.ok(orderInfoService.getOrderList(orderInfoListRequestDTO));
     }
 
     // 배송 정보 수정
-    @PutMapping("/orderlist/modify")
+    @PutMapping("/orderlist/modify?ono={ono}")
     public ResponseEntity<OrderInfoDTO> modifyOrder(@RequestBody OrderInfoDTO orderInfoDTO) {
         return ResponseEntity.ok(orderInfoService.modifyOrder(orderInfoDTO));
     }
 
     // 주문 취소 상태로 변경
-    @PutMapping("/orderlist/{ono}")
+    @PutMapping("/orderlist/cancel?ono={ono}")
     public ResponseEntity<Long> deleteOrder(@PathVariable Long ono) {
         return ResponseEntity.ok(orderInfoService.deleteOrder(ono));
     }
